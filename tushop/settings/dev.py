b42aa8d0386103ae8adfaed07a337679c4bd37de
mwisechacha <angieschacha@gmail.com>
2023-11-07 12:10:21 +0300

    settings

--- tushop/settings/dev.py
+++ tushop/settings/dev.py
@@ -3,6 +3,7 @@ from .common import *
 DEBUG = True
 SECRET_KEY = 'django-insecure-920ltosg7!q)ailxpkji%av^&s)na7p_*eg2(omy=e+109*=+7'
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- tushop/settings/prod.py
+++ tushop/settings/prod.py
@@ -1,8 +1,13 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoplify-prod-5de22bcffe74.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
