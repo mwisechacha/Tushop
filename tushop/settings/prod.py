d6d76eb95d8e25b929675b1389296a5cbec0c8b7
kibzrael <kibraphael7@gmail.com>
2023-11-17 00:21:07 +0300

    compile back changes

--- tushop/settings/prod.py
+++ tushop/settings/prod.py
@@ -13,13 +13,3 @@ ALLOWED_HOSTS = ['shoplify-prod-5de22bcffe74.herokuapp.com', 'shoplify-prod-231a
 DATABASES = {
     'default':dj_database_url.config()
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'qhx06luyg7dzctwo',
-#         'HOST': 't07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-#         'USER': 'jkjsk56die2h2f2b',
-#         'PASSWORD': 'z70ae2d9dw7jprs4',
-#         'PORT': 3306
-#     }
-# }
