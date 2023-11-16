72ece5e686d146fd2253884b2d5d47560be4ac9c
kibzrael <kibraphael7@gmail.com>
2023-11-16 23:53:53 +0300

    database manual config

--- tushop/settings/dev.py
+++ tushop/settings/dev.py
@@ -17,5 +17,13 @@ SECRET_KEY = 'django-insecure-920ltosg7!q)ailxpkji%av^&s)na7p_*eg2(omy=e+109*=+7
 # }
 
 DATABASES = {
-    'default':dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qhx06luyg7dzctwo',
+        'HOST': 't07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'USER': 'jkjsk56die2h2f2b',
+        'PASSWORD': 'z70ae2d9dw7jprs4',
+        'PORT': 3306
+    }
 }
+
