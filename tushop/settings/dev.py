2279cbd127cb947af1643ff1c092cf9e1aaef914
kibzrael <kibraphael7@gmail.com>
2023-11-16 23:40:33 +0300

    dev dj_database_url

--- tushop/settings/dev.py
+++ tushop/settings/dev.py
@@ -1,15 +1,21 @@
+import dj_database_url
+
 from .common import *
 
 DEBUG = True
 SECRET_KEY = 'django-insecure-920ltosg7!q)ailxpkji%av^&s)na7p_*eg2(omy=e+109*=+7'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'tushop',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'rootkristine'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tushop',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'rootkristine'
-    }
+    'default':dj_database_url.config()
 }
