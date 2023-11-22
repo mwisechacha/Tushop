3ce7704e8497c92f59bdfefd4c6f43bfc69e9a75
mwisechacha <angieschacha@gmail.com>
2023-11-22 07:26:44 +0300

    add product images

--- tushop/settings/dev.py
+++ tushop/settings/dev.py
@@ -6,12 +6,23 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-920ltosg7!q)ailxpkji%av^&s)na7p_*eg2(omy=e+109*=+7'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'tushop',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'rootkristine'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tushop',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'rootkristine'
-    }
+        'NAME': 'qhx06luyg7dzctwo',
+        'HOST': 't07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'USER': 'jkjsk56die2h2f2b',
+        'PASSWORD': 'z70ae2d9dw7jprs4',
+        'PORT': 3306
+        }
 }
